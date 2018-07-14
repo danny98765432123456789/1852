386d5b2b8ac796167cc7640a6bac9126bdf3cdd3
Your Name <you@example.com>
2018-07-14 13:38:20 +0800

    update reservation

--- back_end/server_with_mongodb_post.js
+++ back_end/server_with_mongodb_post.js
@@ -6,7 +6,9 @@ mongoose.connection.openUri('mongodb://localhost/1852');
 var mqtt = require('mqtt');
 var bodyParser = require('body-parser');
 var client = mqtt.connect('mqtt://test.mosquitto.org');
-var parseUrlencoded = bodyParser.urlencoded({ extended: false });
+var parseUrlencoded = bodyParser.urlencoded({
+  extended: false
+});
 var reservation_online = require('./model/reservation_online');
 
 
@@ -30,6 +32,170 @@ app.get('/reservation_online', function(request, response) {
   })
 });
 
+app.get('/reservation_online/:date', function(request, response) {
+  let TOTAL_PEOPLE_IN_A_TIME_INTERVAL = 20;
+  reservation_online.find({
+    date: request.params.date
+  }, function(err, result) {
+    let total = {
+      "time1130": 0,
+      "time0100": 0,
+      "time0530": 0,
+      "time0700": 0
+    };
+    for (var i = 0; i < result.length; i++) {
+      add_total_people(total, result[i].time, result[i].adult, result[i].kid);
+    }
+    // console.log(total);
+    // let remaining = TOTAL_PEOPLE_IN_A_TIME_INTERVAL - total;
+    // if (remaining > 0) {
+    //   let temp = [remaining];
+    // response.send(temp);
+    // } else {
+    total.time1130 = 20 - total.time1130;
+    total.time0100 = 20 - total.time0100;
+    total.time0530 = 20 - total.time0530;
+    total.time0700 = 20 - total.time0700;
+    let temp = [total];
+    response.send(temp);
+    // }
+  })
+});
+
+function add_total_people(object, date, adult, kid) {
+  if (date == "1130") {
+    if (adult == 'One') {
+      object.time1130 += 1;
+    }
+    if (adult == 'Two') {
+      object.time1130 += 2;
+    }
+    if (adult == 'Three') {
+      object.time1130 += 3;
+    }
+    if (adult == 'Four') {
+      object.time1130 += 4;
+    }
+    if (adult == 'Five') {
+      object.time1130 += 5;
+    }
+
+    if (kid == 'Zero') {
+      object.time1130 += 5;
+    }
+    if (kid == 'One') {
+      object.time1130 += 1;
+    }
+    if (kid == 'Two') {
+      object.time1130 += 2;
+    }
+    if (kid == 'Three') {
+      object.time1130 += 3;
+    }
+    if (kid == 'Four') {
+      object.time1130 += 4;
+    }
+  }
+  if (date == "0100") {
+    if (adult == 'One') {
+      object.time0100 += 1;
+    }
+    if (adult == 'Two') {
+      object.time0100 += 2;
+    }
+    if (adult == 'Three') {
+      object.time0100 += 3;
+    }
+    if (adult == 'Four') {
+      object.time0100 += 4;
+    }
+    if (adult == 'Five') {
+      object.time0100 += 5;
+    }
+
+    if (kid == 'Zero') {
+      object.time0100 += 5;
+    }
+    if (kid == 'One') {
+      object.time0100 += 1;
+    }
+    if (kid == 'Two') {
+      object.time0100 += 2;
+    }
+    if (kid == 'Three') {
+      object.time0100 += 3;
+    }
+    if (kid == 'Four') {
+      object.time0100 += 4;
+    }
+  }
+  if (date == "0530") {
+    if (adult == 'One') {
+      object.time0530 += 1;
+    }
+    if (adult == 'Two') {
+      object.time0530 += 2;
+    }
+    if (adult == 'Three') {
+      object.time0530 += 3;
+    }
+    if (adult == 'Four') {
+      object.time0530 += 4;
+    }
+    if (adult == 'Five') {
+      object.time0530 += 5;
+    }
+
+    if (kid == 'Zero') {
+      object.time0530 += 5;
+    }
+    if (kid == 'One') {
+      object.time0530 += 1;
+    }
+    if (kid == 'Two') {
+      object.time0530 += 2;
+    }
+    if (kid == 'Three') {
+      object.time0530 += 3;
+    }
+    if (kid == 'Four') {
+      object.time0530 += 4;
+    }
+  }
+  if (date == "0700") {
+    if (adult == 'One') {
+      object.time0700 += 1;
+    }
+    if (adult == 'Two') {
+      object.time0700 += 2;
+    }
+    if (adult == 'Three') {
+      object.time0700 += 3;
+    }
+    if (adult == 'Four') {
+      object.time0700 += 4;
+    }
+    if (adult == 'Five') {
+      object.time0700 += 5;
+    }
+
+    if (kid == 'Zero') {
+      object.time0700 += 5;
+    }
+    if (kid == 'One') {
+      object.time0700 += 1;
+    }
+    if (kid == 'Two') {
+      object.time0700 += 2;
+    }
+    if (kid == 'Three') {
+      object.time0700 += 3;
+    }
+    if (kid == 'Four') {
+      object.time0700 += 4;
+    }
+  }
+}
 // app.get('/packets/:src', function(request, response) {
 //   Packet.find({
 //     src: request.params.src
@@ -83,8 +249,7 @@ app.post('/reservation_online', parseUrlencoded, function(request, response) {
   newjson.save(function(err) {
     if (!err) {
       response.send("Success!");
-    }
-    else{
+    } else {
       console.log(err);
     }
   });
--- front_end/js/reservation.js
+++ front_end/js/reservation.js
@@ -1,8 +1,66 @@
 $(document).ready(function() {
-
+  let temp_adult = 0;
+  let temp_kid = 0;
+  let flag1 = 0;
+  let flag2 = 0;
   process_drop();
   process_drop1();
   process_sumit();
+  process_time_button();
+  // setInterval(function() {
+  //   generate_valid_time();
+  // }, 1000);
+
+  function process_time_button() {
+    $("#1130").click(function() {
+      temp_time_button = "1130";
+      alert(temp_time_button);
+    });
+    $("#0100").click(function() {
+      temp_time_button = "0100";
+    });
+    $("#0530").click(function() {
+      temp_time_button = "0530";
+    });
+    $("#0700").click(function() {
+      temp_time_button = "0700";
+    });
+  }
+
+  function generate_valid_time() {
+    $("#1130").hide(1000);
+    $("#0100").hide(1000);
+    $("#0530").hide(1000);
+    $("#0700").hide(1000);
+    if (remaining_obj.time1130 >= temp_total) {
+      // $("#1130").hide(1000);
+      // $("#0100").hide(1000);
+      // $("#0530").hide(1000);
+      // $("#0700").hide(1000);
+      $("#1130").show(1000);
+    }
+    if (remaining_obj.time0100 >= temp_total) {
+      // $("#1130").hide(1000);
+      // $("#0100").hide(1000);
+      // $("#0530").hide(1000);
+      // $("#0700").hide(1000);
+      $("#0100").show(1000);
+    }
+    if (remaining_obj.time0530 >= temp_total) {
+      // $("#1130").hide(1000);
+      // $("#0100").hide(1000);
+      // $("#0530").hide(1000);
+      // $("#0700").hide(1000);
+      $("#0530").show(1000);
+    }
+    if (remaining_obj.time0700 >= temp_total) {
+      // $("#1130").hide(1000);
+      // $("#0100").hide(1000);
+      // $("#0530").hide(1000);
+      // $("#0700").hide(1000);
+      $("#0700").show(1000);
+    }
+  }
 
   function process_sumit() {
     // var oform = document.forms['form'];
@@ -12,7 +70,12 @@ $(document).ready(function() {
     // var coor2 = oform.elements['coor2'].value;
     // var coor6 = oform.elements['coor6'].value;
     $("#buttonsubmit").click(function() {
-      alert("123123123")
+
+      const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+      ];
+      temp_month = monthNames[datefromcalendar.slice(5, 7)-1];
+      // alert("123123123");
       var oform = document.forms['form'];
       var name = oform.elements['name'].value;
       var phone = oform.elements['phone'].value;
@@ -20,8 +83,8 @@ $(document).ready(function() {
       var note = oform.elements['note'].value;
       // alert(phone.toString()+ email.toString()+ note.toString()+ datefromcalendar+ adultfromdrop+ kidfromdrop);
       // alert(email.toString());
-      alert(name.toString());
-      alert(phone.toString());
+      // alert(name.toString());
+      // alert(phone.toString());
       $.ajax({
         type: 'POST',
         url: 'http://localhost:3000/reservation_online',
@@ -32,10 +95,11 @@ $(document).ready(function() {
           note: note.toString(),
           date: datefromcalendar,
           adult: adultfromdrop,
-          kid: kidfromdrop
+          kid: kidfromdrop,
+          time: temp_time_button
         },
         success: function(data) {
-          alert("Success!!");
+          alert("您已訂位成功!!\nYou have successfully booked the seats!!");
         },
         error: function(err) {
           alert(err);
@@ -49,6 +113,11 @@ $(document).ready(function() {
 
   function process_drop1() {
     $(".drop1 .option").click(function() {
+      if (flag2 == 0) {
+        flag2 = 1
+      } else {
+        flag2 = 0
+      }
       var val1 = $(this).attr("data-value"),
         $drop = $(".drop1"),
         prevActive = $(".drop .option.active").attr("data-value"),
@@ -65,6 +134,29 @@ $(document).ready(function() {
         }, 400 + options * 100);
       }
       kidfromdrop = val1;
+      if (kidfromdrop == 'placeholder') {
+        temp_kid = 0;
+      }
+      if (kidfromdrop == 'Zero') {
+        temp_kid = 0;
+      }
+      if (kidfromdrop == 'One') {
+        temp_kid = 1;
+      }
+      if (kidfromdrop == 'Two') {
+        temp_kid = 2;
+      }
+      if (kidfromdrop == 'Three') {
+        temp_kid = 3;
+      }
+      if (kidfromdrop == 'Four') {
+        temp_kid = 4;
+      }
+      temp_total = temp_adult + temp_kid;
+      if (!flag2) {
+        generate_valid_time()
+      }
+      // alert(temp_total);
       triggerAnimation1();
       if (val1 !== "placeholder" || prevActive === "placeholder") {
         $(".drop1 .option").removeClass("active");
@@ -75,6 +167,11 @@ $(document).ready(function() {
 
   function process_drop() {
     $(".drop .option").click(function() {
+      if (flag1 == 0) {
+        flag1 = 1
+      } else {
+        flag1 = 0
+      }
       var val = $(this).attr("data-value"),
         $drop = $(".drop"),
         prevActive = $(".drop .option.active").attr("data-value"),
@@ -92,6 +189,27 @@ $(document).ready(function() {
       }
       // alert(val);
       adultfromdrop = val;
+      if (adultfromdrop != "placeholder") {
+        if (adultfromdrop == 'One') {
+          temp_adult = 1;
+        }
+        if (adultfromdrop == 'Two') {
+          temp_adult = 2;
+        }
+        if (adultfromdrop == 'Three') {
+          temp_adult = 3;
+        }
+        if (adultfromdrop == 'Four') {
+          temp_adult = 4;
+        }
+        if (adultfromdrop == 'Five') {
+          temp_adult = 5;
+        }
+      }
+      temp_total = temp_adult + temp_kid;
+      if (!flag1) {
+        generate_valid_time()
+      } // alert(temp_total);
       triggerAnimation();
       if (val !== "placeholder" || prevActive === "placeholder") {
         $(".drop .option").removeClass("active");
